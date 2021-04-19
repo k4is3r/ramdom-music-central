6036baa2242579e15bf3303cb2744ce8a00e0b06
Eduardo Imery <ejwimery@gmail.com>
2021-04-18 21:51:18 -0600

    creating a functions for generate unique rooms codes

--- music_center/api/models.py
+++ music_center/api/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from string
+from random
+
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = ''.join(random.choice(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    
+    return code
+    
 
 class Room(models.Model):
     code = models.CharField(max_length=8, default="", unique=True)
