61ed86f8a58c5313100d799500993a1835c19a70
Eduardo Imery <ejwimery@gmail.com>
2021-04-19 20:24:05 -0600

    creating all the logic to show romm created, FE BE

--- music_center/frontend/src/components/HomePage.js
+++ music_center/frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
 import { 
     BrowserRouter as Router, 
     Switch, 
@@ -18,9 +19,10 @@ export default class HomePage extends Component {
     render() {
         return <Router>
             <Switch>
-                <Route  exact path='/'><p>This is the Home Page</p></Route>
-                <Route path='/join' component={RoomJoinPage} />
-                <Route path='/create' component={CreateRoomPage} />
+                <Route  exact path="/"><p>This is the Home Page</p></Route>
+                <Route path="/join" component={RoomJoinPage} />
+                <Route path="/create" component={CreateRoomPage} />
+                <Route path="/room/:roomCode" component={Room} />
             </Switch>
         </Router>
     }
