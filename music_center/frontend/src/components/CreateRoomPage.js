235a1384982bfbe15ec79fe00828b657d9db66be
Eduardo Imery <ejwimery@gmail.com>
2021-04-19 18:41:09 -0600

    creating FE for create room

--- music_center/frontend/src/components/CreateRoomPage.js
+++ music_center/frontend/src/components/CreateRoomPage.js
@@ -1,12 +1,53 @@
 import React, {Component} from 'react';
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { Link } from "react-router-dom";
+
 
 export default class CreateRoomPage extends Component {
+    defaulVotes = 2;
+
     constructor(props){
         super(props);
     }
 
     render() {
-        return <p>This is the CreateRoomPage</p>;
+        return(
+            <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <Typography component="h4" variant="h4">
+                    Create A Room
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <FormControl component="fieldset">
+                    <FormHelperText>
+                        <div align="center">Guest Control of Playback State</div>
+                    </FormHelperText>
+                    <RadioGroup row defaultValue="true">
+                        <FormControlLabel 
+                            value="true" 
+                            control={<Radio color="primary"/>}
+                            label="Play/Pause"
+                            labelPlacement="bottom"
+                        />
+                        <FormControlLabel 
+                            value="false" 
+                            control={<Radio color="secondary"/>}
+                            label="No Control"
+                            labelPlacement="bottom"
+                        />
+                    </RadioGroup>
+                </FormControl>
+            </Grid>
+        </Grid>
+        );
     }
 }
 
