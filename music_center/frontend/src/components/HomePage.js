6fb3dbd6a08542ced0fef0349cd261bbb6145de1
Eduardo Imery <ejwimery@gmail.com>
2021-04-21 17:42:00 -0600

    re-disgn HomePage

--- music_center/frontend/src/components/HomePage.js
+++ music_center/frontend/src/components/HomePage.js
@@ -9,17 +9,60 @@ import {
     Link, 
     Redirect
 } from "react-router-dom";
-
+import {
+    Grid,
+    Button,
+    ButtonGroup,
+    Typography
+} from "@material-ui/core"
 
 export default class HomePage extends Component {
     constructor(props){
         super(props);
     }
 
+    renderHomePage(){
+        return(
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" compact="h3">
+                        Music Center
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup
+                        disableElevation
+                        variant="contained"
+                        color="primary"
+                    >
+                        <Button
+                            color="primary"
+                            to='/join'
+                            component={Link}
+
+                        >
+                            Join Room
+                        </Button>
+                        <Button
+                            color="secondary"
+                            to='/create'
+                            component={Link}
+                            
+                        >
+                            Create Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
+    }
+
     render() {
         return <Router>
             <Switch>
-                <Route  exact path="/"><p>This is the Home Page</p></Route>
+                <Route  exact path="/">
+                    {this.renderHomePage()}
+                </Route>
                 <Route path="/join" component={RoomJoinPage} />
                 <Route path="/create" component={CreateRoomPage} />
                 <Route path="/room/:roomCode" component={Room} />
